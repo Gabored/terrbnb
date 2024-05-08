15313aff8296983bc2f4b4eeb86f554530a9fb93
Octavio Valdez <90404916+OctavioValdez@users.noreply.github.com>
2024-05-07 23:17:15 -0500

    post and reserve js

--- src/public/scripts/post.js
+++ src/public/scripts/post.js
@@ -3,9 +3,7 @@ const token  = localStorage.getItem('token');
 document.addEventListener('DOMContentLoaded', async function () {
     const urlPosts = './post';
 
-    let div_post = document.createElement('post-container');
-    let data_posts = undefined;
-
+    let div_post = document.getElementById ('post-container');
 
     let resp = await fetch(urlPosts, {
         method: 'GET',
@@ -15,6 +13,34 @@ document.addEventListener('DOMContentLoaded', async function () {
     });
 
     let posts = await resp.json();
+    console.log(posts);
 
-    console.log("posts", posts);
+    posts.forEach(post => {
+        const card = document.createElement('div');
+        card.classList.add('card');
+
+        card.innerHTML = `
+        <h2>${post.title}</h2>
+        <p>${post.description}</p>
+        <a href="/reserve"><button id="${post.idProperty}" class="button-reserve">Reservar</button></a>
+        `
+
+        div_post.appendChild(card);
+    });
+
+    const buttons = document.querySelectorAll('.button-reserve');
+
+    buttons.forEach(button => {
+        button.addEventListener('click', handleClick);
+    })
 })
+
+
+function handleClick(event) {
+    const clicked_button = event.target.closest('button');
+    let button_id = clicked_button.getAttribute('id');
+
+    sessionStorage.setItem('id-property',button_id);
+
+}
+
